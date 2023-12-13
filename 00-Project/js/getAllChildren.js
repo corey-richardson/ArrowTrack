3e50cab073301c13d4270aea65d502fafc28cdee
Corey Richardson <richardsoncorey04@gmail.com>
2023-12-13 13:08:43 +0000

    Still WIP; promises?

--- 00-Project/js/getAllChildren.js
+++ 00-Project/js/getAllChildren.js
@@ -1,10 +1,45 @@
-function getAllChildren() {
-
+function promiseWrapper()
+{
+    testGetAllChildren();
+    
     fetch('./static/items.json')
-        .then(response => response.json())
+        .then(response => response.json()) // http 200 OK
         .then(json => {
-            json.sort((a, b) => (a.brand > b.brand ? 1 : -1));
             console.log(json);
             return json;
         });
 }
+
+async function getAllChildren () 
+{
+    let data = await promiseWrapper(); 
+    console.log(data);
+    return data;
+}
+
+function testGetAllChildren() {
+
+    const testData = [
+        {
+            "brand":"PSE Stinger ATK",
+            "condition":"Good",
+            "value":327.00,
+            "quantity":1,
+            "location":"Widewell",
+            "notes":"Compound 50# Right Handed"
+        },
+        {
+            "brand":"Easton X7",
+            "condition":"Good",
+            "value":15.00,
+            "quantity":8,
+            "location":"YMCA",
+            "notes":"28.5\" 2014"
+        }
+    ]
+
+    console.log(testData);
+    return testData;
+}
+
+// https://stackoverflow.com/questions/39021870/fetch-returns-promise-instead-of-actual-data-even-after-using-then
