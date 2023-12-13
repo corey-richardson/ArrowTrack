a3d88ed3d82fc00f286b6c02df8cecd20ae90a0c
Corey Richardson <richardsoncorey04@gmail.com>
2023-12-13 15:01:37 +0000

    Revert

--- 00-Project/js/getAllChildren.js
+++ 00-Project/js/getAllChildren.js
@@ -66,36 +66,4 @@ function getAllChildren() {
     return testData;
 }
 
-async function getAllChildren () 
-{
-    let data = await promiseWrapper(); 
-    console.log(data);
-    return data;
-}
-
-function testGetAllChildren() {
-
-    const testData = [
-        {
-            "brand":"PSE Stinger ATK",
-            "condition":"Good",
-            "value":327.00,
-            "quantity":1,
-            "location":"Widewell",
-            "notes":"Compound 50# Right Handed"
-        },
-        {
-            "brand":"Easton X7",
-            "condition":"Good",
-            "value":15.00,
-            "quantity":8,
-            "location":"YMCA",
-            "notes":"28.5\" 2014"
-        }
-    ]
-
-    console.log(testData);
-    return testData;
-}
-
 // https://stackoverflow.com/questions/39021870/fetch-returns-promise-instead-of-actual-data-even-after-using-then
--- 00-Project/js/view.js
+++ 00-Project/js/view.js
@@ -1,32 +1,27 @@
-async function view() {
-    const viewElements = await getAllChildren();
-    console.log(viewElements);
+const viewElements = getAllChildren();
 
-    let length = viewElements.length;
+let length = viewElements.length;
 
-    const viewContainer = document.getElementById("view-container"); 
+const viewContainer = document.getElementById("view-container"); 
 
-    for (let i = 0; i < length; i++) // for each elem
-    {
-        let text = "";
+for (let i = 0; i < length; i++) // for each elem
+{
+    let text = "";
 
-        // Common Parsing Rules
-        viewElements[i].value = "£" + viewElements[i].value.toFixed(2);
+    // Common Parsing Rules
+    viewElements[i].value = "£" + viewElements[i].value.toFixed(2);
 
-        // Create HTML string
-        text = text
-        + "<h2>" + viewElements[i].brand + "</h2>"
-        + "<h4> - " + viewElements[i].location + ", " + viewElements[i].value + " x " + viewElements[i].quantity + "</h4>"
-        + "<br><p>" + viewElements[i].condition + " condtion, " + viewElements[i].notes + "</p>" 
-        + "<style>h2, h4 { display: inline; } h4 { font-weight: normal; } </style>";
+    // Create HTML string
+    text = text
+    + "<h2>" + viewElements[i].brand + "</h2>"
+    + "<h4> - " + viewElements[i].location + ", " + viewElements[i].value + " x " + viewElements[i].quantity + "</h4>"
+    + "<br><p>" + viewElements[i].condition + " condtion, " + viewElements[i].notes + "</p>" 
+    + "<style>h2, h4 { display: inline; } h4 { font-weight: normal; } </style>";
 
-        // Create DOM object
-        const elemContainer = document.createElement("div");
-        elemContainer.classList.add("elem-container");
-        // Add created DOM element to view-container
-        elemContainer.innerHTML = text;
-        viewContainer.appendChild(elemContainer);
-    }
+    // Create DOM object
+    const elemContainer = document.createElement("div");
+    elemContainer.classList.add("elem-container");
+    // Add created DOM element to view-container
+    elemContainer.innerHTML = text;
+    viewContainer.appendChild(elemContainer);
 }
-
-view();
