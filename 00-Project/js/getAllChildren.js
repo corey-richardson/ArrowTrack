463aa1fdb5c48b2f08259833e7b17d96e18586ac
Corey Richardson <richardsoncorey04@gmail.com>
2023-12-13 14:53:45 +0000

    Revert main to before JSON file reading change

--- 00-Project/js/getAllChildren.js
+++ 00-Project/js/getAllChildren.js
@@ -1,67 +1,45 @@
-function getAllChildren() {
+function promiseWrapper()
+{
+    testGetAllChildren();
+    
+    fetch('./static/items.json')
+        .then(response => response.json()) // http 200 OK
+        .then(json => {
+            console.log(json);
+            return json;
+        });
+}
+
+async function getAllChildren () 
+{
+    let data = await promiseWrapper(); 
+    console.log(data);
+    return data;
+}
+
+function testGetAllChildren() {
 
     const testData = [
         {
-            brand:"PSE Stinger ATK",
-            condition:"Good",
-            value:327.00,
-            quantity:1,
-            location:"Widewell",
-            notes:"Compound 50# Right Handed"
+            "brand":"PSE Stinger ATK",
+            "condition":"Good",
+            "value":327.00,
+            "quantity":1,
+            "location":"Widewell",
+            "notes":"Compound 50# Right Handed"
         },
         {
-            brand:"Easton X7",
-            condition:"Good",
-            value:15.00,
-            quantity:8,
-            location:"YMCA",
-            notes:"28.5\" 2014"
-        },
-        {
-            brand:"Easton ACC",
-            condition:"Good",
-            value:18,
-            quantity:10,
-            location:"Widewell",
-            notes:"28.5\" 630"
-        },
-        {
-            brand:"WA 60cm Face",
-            condition:"Used",
-            value:0.28,
-            quantity:25,
-            location:"YMCA",
-            notes:"Three Spot"
-        },
-        {
-            brand:"A-Frame Stand",
-            condition:"Okay",
-            value:35.00,
-            quantity:5,
-            location:"Widewell",
-            notes:""
-        },
-        {
-            brand:"Bowmaster Portable Bow Press",
-            condition:"Good",
-            value:100.00,
-            quantity:1,
-            location:"Widewell",
-            notes:""
-        },
-        {
-            type:"PPE",
-            brand:"Arm Guard",
-            condition:"Okay",
-            value:5.00,
-            quantity:10,
-            location:"Widewell",
-            notes:""
+            "brand":"Easton X7",
+            "condition":"Good",
+            "value":15.00,
+            "quantity":8,
+            "location":"YMCA",
+            "notes":"28.5\" 2014"
         }
-        
-    ];
+    ]
 
-    testData.sort((a, b) => (a.brand > b.brand ? 1 : -1));
-
+    console.log(testData);
     return testData;
 }
+
+// https://stackoverflow.com/questions/39021870/fetch-returns-promise-instead-of-actual-data-even-after-using-then
