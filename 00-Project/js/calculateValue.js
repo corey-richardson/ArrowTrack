c0ff972940848c91619d7b2ace6dbcea6022c709
Corey Richardson <richardsoncorey04@gmail.com>
2023-12-13 16:11:44 +0000

    Moved logic into .then() blocks to get around asynchronous nature of browser file io

--- 00-Project/js/calculateValue.js
+++ 00-Project/js/calculateValue.js
@@ -1,55 +1,61 @@
-const jsonElements = getAllChildren();
+fetch("./static/items.json")
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(jsonElements => {
+        console.log(jsonElements);
+        // _all is prefixed with an underscore as it should not be displayed to the user
+        // the underscore should hopefully be immeadiatley obvious on the webpage if this rule is not followed
+        const locations = {"_all" : 0.0};
 
-let sum = 0.0;
+        for (let obj in jsonElements)
+        {
+            let object = jsonElements[obj];
+            if (!locations[object.location])
+            {
+                locations[object.location] = 0.0;
+            }
+        }
+        console.log(locations);
 
-// _all is prefixed with an underscore as it should not be displayed to the user
-// the underscore should hopefully be immeadiatley obvious on the webpage if this rule is not followed
-const locations = {"_all" : 0.0};
+        for (let obj in jsonElements) {
+            let object = jsonElements[obj];
+            
+            // Set default value if not already set
+            if (!object.quantity)
+            {
+                object.quantity = 1;
+            }
 
-for (let obj in jsonElements)
-{
-    let object = jsonElements[obj];
-    if (!locations[object.location])
-    {
-        locations[object.location] = 0.0;
-    }
-}
-console.log(locations);
+            // Update values
+            locations["_all"] += object.value * object.quantity;
+            locations[object.location] += object.value * object.quantity;
+        }
 
-for (let obj in jsonElements) {
-    let object = jsonElements[obj];
-    
-    // Set default value if not already set
-    if (!object.quantity)
-    {
-        object.quantity = 1;
-    }
+        // Temp buffer
+        let text = "";
 
-    // Update values
-    locations["_all"] += object.value * object.quantity;
-    locations[object.location] += object.value * object.quantity;
-}
+        // For each storage location
+        for (let loc in locations)
+        {
+            if (loc == "_all") // only display sum, not name
+            {
+                text += "£" + locations["_all"].toFixed(2);
+                continue;
+            }
 
-// Temp buffer
-let text = "";
+            text += " | " + loc + ": £" + locations[loc].toFixed(2);
+            console.log("Here")
+        }
 
-// For each storage location
-for (let loc in locations)
-{
-    if (loc == "_all") // only display sum, not name
-    {
-        text += "£" + locations["_all"].toFixed(2);
-        continue;
-    }
+        // Create DOM object and style it
+        const para = document.createElement("p");
+        para.style.textAlign = "center";
+        para.style.fontWeight = "bold";
+        // Add created DOM element to view-page
+        para.innerHTML = text;
+        const vp = document.getElementById("view-page");
+        vp.appendChild(para);
+    });
 
-    text += " | " + loc + ": £" + locations[loc].toFixed(2);
-}
-
-// Create DOM object and style it
-const para = document.createElement("p");
-para.style.textAlign = "center";
-para.style.fontWeight = "bold";
-// Add created DOM element to view-page
-para.innerHTML = text;
-const vp = document.getElementById("view-page");
-vp.appendChild(para);
