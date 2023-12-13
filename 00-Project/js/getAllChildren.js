b77931303311b8f0fe108ad32225280edbce6232
Corey Richardson <richardsoncorey04@gmail.com>
2023-12-13 14:59:25 +0000

    Revert "WIP"
    This reverts commit 1897ed671252680183ddbe5d7014bb141bdfe8ba.

--- 00-Project/js/getAllChildren.js
+++ 00-Project/js/getAllChildren.js
@@ -1,13 +1,69 @@
-function promiseWrapper()
-{
-    testGetAllChildren();
-    
-    fetch('./static/items.json')
-        .then(response => response.json()) // http 200 OK
-        .then(json => {
-            console.log(json);
-            return json;
-        });
+function getAllChildren() {
+
+    const testData = [
+        {
+            brand:"PSE Stinger ATK",
+            condition:"Good",
+            value:327.00,
+            quantity:1,
+            location:"Widewell",
+            notes:"Compound 50# Right Handed"
+        },
+        {
+            brand:"Easton X7",
+            condition:"Good",
+            value:15.00,
+            quantity:8,
+            location:"YMCA",
+            notes:"28.5\" 2014"
+        },
+        {
+            brand:"Easton ACC",
+            condition:"Good",
+            value:18,
+            quantity:10,
+            location:"Widewell",
+            notes:"28.5\" 630"
+        },
+        {
+            brand:"WA 60cm Face",
+            condition:"Used",
+            value:0.28,
+            quantity:25,
+            location:"YMCA",
+            notes:"Three Spot"
+        },
+        {
+            brand:"A-Frame Stand",
+            condition:"Okay",
+            value:35.00,
+            quantity:5,
+            location:"Widewell",
+            notes:""
+        },
+        {
+            brand:"Bowmaster Portable Bow Press",
+            condition:"Good",
+            value:100.00,
+            quantity:1,
+            location:"Widewell",
+            notes:""
+        },
+        {
+            type:"PPE",
+            brand:"Arm Guard",
+            condition:"Okay",
+            value:5.00,
+            quantity:10,
+            location:"Widewell",
+            notes:""
+        }
+        
+    ];
+
+    testData.sort((a, b) => (a.brand > b.brand ? 1 : -1));
+
+    return testData;
 }
 
 async function getAllChildren () 
