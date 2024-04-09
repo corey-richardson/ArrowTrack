26762e1c74c0c27e295b2c95d2a42cc5809ee650
Corey Richardson <richardsoncorey04@gmail.com>
2024-04-09 12:51:30 +0100

    Changes to conditional displays / dynamic CSS styling

--- 00-Project/js/view.js
+++ 00-Project/js/view.js
@@ -11,16 +11,39 @@ if (storedItems) // db not empty
             "<div id='viewContainer" + i + "'>"
             + "<h2>" + storedItems[i].name + "</h2>"
             + "<h4> - " + storedItems[i].location + ",  £" + storedItems[i].value.toFixed(2) + " x " + storedItems[i].quantity + "</h4>"
-            + "<br><p>" + storedItems[i].condition + " condtion, " + storedItems[i].notes + "</p>" 
+            + "<br><p>" + storedItems[i].condition + " condtion</p>" 
+            + "<p class='note-p'>" + storedItems[i].notes + "</p>" 
             + "<style>h2, h4 { display: inline; } h4 { font-weight: normal; } </style>"
-            + "<hr></div>"
+            + "</div><hr>"
         ));
+
+        // Only display Notes attribute paragraph if it contains information
+        if (storedItems[i].notes.length == 0)
+        {
+            $("viewContainer" + i + " > .note-p").css({
+                "display" : "none"
+            });
+        }
         
         // Modify the background colour of any "checked out" items
         if (storedItems[i].checkedOut == "true")
         {
             $("#viewContainer" + i).css({
                 "background-color" : "#a1a1a1"
+            });
+        }
+
+        // Modify the backgroun colour (and relevant borders) for any items marked
+        // as "Unsafe"; highlight danger!
+        if (storedItems[i].condition == "Unsafe")
+        {
+            console.log(storedItems[i].condition);
+
+            $("#viewContainer" + i).css({
+                "background-color" : "#FF000090",
+                "border-radius" : "6px",
+                "padding" : "3px",
+                "margin-bottom" : "3px"
             });
         }
     }
